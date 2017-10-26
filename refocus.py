77dc9034f41256283a5b35a98e849a42e42d6ce5
cigarbeer <r06921038@ntu.edu.tw>
2017-10-27 06:17:35 +0800

    res

--- refocus.py
+++ refocus.py
@@ -132,7 +132,8 @@ def calcBoundaryMap(boundary):
 FLOWERFOCUSINDEX = [0, 1, 4, 8]
 ARTFOCUSINDEX = [4, 5, 6, 7, 9]
 JADEPLANTFOCUSINDEX = [3, 5, 6, 7, 8, 9]
-DOFLEVEL = [0.7, 1.2, 1.5]
+# DOFLEVEL = [0.7, 1.2, 1.5]
+DOFLEVEL = [2]
 DEPTHLAYER = 10
 
 
